5178b306f0e868322aec26d28995dd07f4d78aae
Xavier Caron <xcaron@gmail.com>
2015-05-31 17:09:17 +0200

    enter index.js + assets

--- lib/App/Nellou.pm
+++ lib/App/Nellou.pm
@@ -7,4 +7,10 @@ get '/' => sub {
     template 'index';
 };
 
+# get '/assets/:file.png' => sub {
+#   my $file = param 'file';
+#
+#   return send_file ( "/assets/$file.png", content_type => 'image/png', "$file.png" );
+# };
+
 true;
